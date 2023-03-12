5988f88fc118d36b2b7bbfbd050ef675e3d56e6b
asutoshgha <asutoshghanto23@gmail.com>
2023-03-13 00:40:37 +0530

    added buckets

--- handler.py
+++ handler.py
@@ -2,8 +2,8 @@ from boto3 import client as boto3_client
 import face_recognition
 import pickle
 
-input_bucket = "546proj2"
-output_bucket = "546proj2output"
+input_bucket = "inputbucket-767282627159"
+output_bucket = "lambdafunction-1-outputbucket-1r728zuzpyzl6"
 
 # Function to read the 'encoding' file
 def open_encoding(filename):
